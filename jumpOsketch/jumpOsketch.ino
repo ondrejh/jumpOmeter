0885b0f4fc45b3a623f4fb1aa9dd6d088ba74581
Ondrej Hejda <ondrejh.ck@email.cz>
2020-06-02 18:27:00 +0200

    improved limits

--- jumpOsketch/jumpOsketch.ino
+++ jumpOsketch/jumpOsketch.ino
@@ -6,14 +6,22 @@
 #define ECHO_RESP_PIN D7
 
 #define MEASUREMENT_PERIOD 20 //ms
-#define MESS_LONG 512 // long averaging
-#define MESS_SHORT 8 // short averaging
+#define MESS_MAX 500
+#define MESS_LONG 100 // long averaging
+#define MESS_SHORT 10 // short averaging
 
 #define BOUNCE_THR 0.9 // bounce threshold
 #define BOUNCE_CNT_MIN 3
 #define BOUNCE_CNT_MAX 30
 
-uint8_t data[MESS_LONG];
+#define BEEP false
+#define BEEP_PIN D5
+#define BEEP_FREQV 880
+#define BEEP_DUR 100
+
+uint8_t data[MESS_MAX];
+float l_avg[MESS_MAX];
+float s_avg[MESS_MAX];
 
 // function to measure ultrasonic echo
 uint8_t get_echo(void) {
@@ -33,7 +41,9 @@ void setup() {
   pinMode(ECHO_RESP_PIN, INPUT);
 
   // init data
-  memset(data, 0, MESS_LONG);
+  memset(data, 0, MESS_MAX);
+  memset(l_avg, 0, MESS_MAX * sizeof(float));
+  memset(s_avg, 0, MESS_MAX * sizeof(float));
   
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
@@ -50,31 +60,39 @@ void loop() {
     uint8_t e = get_echo();
     static int p = 0;
     static uint32_t longavg = 0;
-    longavg = longavg - data[p] + e;
-    int pp = p - MESS_SHORT;
-    if (pp < 0) pp += MESS_LONG;
     static uint32_t shortavg = 0;
+    
+    int pp = p - MESS_LONG;
+    if (pp < 0) pp += MESS_MAX;
+    longavg = longavg - data[pp] + e;
+    
+    pp = p - MESS_SHORT;
+    if (pp < 0) pp += MESS_MAX;
     shortavg = shortavg - data[pp] + e;
-    data[p++] = e;
-    if (p >= MESS_LONG) p = 0;
-    float lavg = (float)longavg / MESS_LONG;
-    float savg = (float)shortavg / MESS_SHORT;
+
+    data[p] = e;
+    l_avg[p] = (float)longavg / MESS_LONG;
+    s_avg[p] = (float)shortavg / MESS_SHORT;
 
     static int bnc = 0;
     bool b = false;
-    if (savg < (lavg * BOUNCE_THR)) {
+    if (s_avg[p] < (l_avg[p] * BOUNCE_THR)) {
       bnc ++;
     }
-    if (savg > lavg) {
+    if (s_avg[p] > l_avg[p]) {
       if ((bnc >= BOUNCE_CNT_MIN) && (bnc < BOUNCE_CNT_MAX)) b = true;
       bnc = 0;
     }
-    Serial.print(lavg);
+    Serial.print(l_avg[p]);
     Serial.print(" ");
-    Serial.print(savg);
-    Serial.print(" ");
-    Serial.print(e);
-    if (b) Serial.print(" bounce");
+    Serial.print(s_avg[p]);
+    if (b) {
+      Serial.print(" bounce");
+      if (BEEP) tone(BEEP_PIN, BEEP_FREQV, BEEP_DUR);
+    }
     Serial.println();
+    
+    p++;
+    if (p >= MESS_MAX) p = 0;
   }
 }
