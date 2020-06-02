caa559a2ac0258cf85b54b97cbaa652661772a90
Ondrej Hejda <ondrejh.ck@email.cz>
2020-06-02 21:43:11 +0200

    first webi, data output

--- jumpOsketch/jumpOsketch.ino
+++ jumpOsketch/jumpOsketch.ino
@@ -2,11 +2,25 @@
   Jump-O-Meter
 */
 
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+#include <EEPROM.h>
+
+#include "webi.h"
+
 #define ECHO_TRIG_PIN D6
 #define ECHO_RESP_PIN D7
 
+#define WLED BUILTIN_LED
+
 #define MEASUREMENT_PERIOD 20 //ms
-#define MESS_MAX 500
+#define MESS_MAX 501
 #define MESS_LONG 100 // long averaging
 #define MESS_SHORT 10 // short averaging
 
@@ -19,9 +33,20 @@
 #define BEEP_FREQV 880
 #define BEEP_DUR 100
 
+#define ON LOW
+#define OFF HIGH
+
 uint8_t data[MESS_MAX];
 float l_avg[MESS_MAX];
 float s_avg[MESS_MAX];
+bool bncs[MESS_MAX];
+int p = 0;
+
+// wifi ap settings
+const char *ssid = "JumpOMeter";
+const char *password = "jump0meter";
+
+ESP8266WebServer server(80);
 
 // function to measure ultrasonic echo
 uint8_t get_echo(void) {
@@ -31,6 +56,55 @@ uint8_t get_echo(void) {
   uint32_t e = pulseIn(ECHO_RESP_PIN, HIGH, 0x3FFF);
   if (e == 0) return (0x3FFF >> 5);
   return (e >> 5);
+}
+
+// web server handlers
+void handleRoot() {
+  digitalWrite(WLED, ON);
+  server.send(200, "text/html", index_html);
+  digitalWrite(WLED, OFF);
+}
+
+void handleData() {
+  digitalWrite(WLED, ON);
+  server.sendHeader("Access-Control-Max-Age", "10000");
+  server.sendHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  server.sendHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  String s;
+  s += "{\"x\":[";
+  bool first = true;
+  for (int i=0; i<MESS_MAX; i++) {
+    if (first) first = false; else s += ",";
+    s += String(i * MEASUREMENT_PERIOD);
+  }
+  s += "],\"f\":[";
+  first = true;
+  int pp = p;
+  for (int i=0; i<MESS_MAX; i++) {
+    if (first) first = false; else s += ",";
+    s += String(l_avg[pp++]);
+    if (pp >= MESS_MAX) pp = 0;
+  }
+  s += "],\"s\":[";
+  first = true;
+  pp = p;
+  for (int i=0; i<MESS_MAX; i++) {
+    if (first) first = false; else s += ",";
+    s += String(s_avg[pp++]);
+    if (pp >= MESS_MAX) pp = 0;
+  }
+  s += "],\"b\":[";
+  first = true;
+  pp = p;
+  for (int i=0; i<MESS_MAX; i++) {
+    if (first) first = false; else s += ",";
+    s += bncs[pp++]?"1":"0";
+    if (pp >= MESS_MAX) pp = 0;
+  }
+  s += "]}";
+  server.send(200, "application/json", s);
+  digitalWrite(WLED, OFF);
 }
 
 // the setup function runs once when you press reset or power the board
@@ -46,19 +120,35 @@ void setup() {
   memset(s_avg, 0, MESS_MAX * sizeof(float));
   
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(WLED, OUTPUT);
+  digitalWrite(WLED, OFF);
 
   // init serial
   Serial.begin(115200);
+
+  // start the access point
+  IPAddress ip(192, 168, 42, 1);
+  IPAddress subnet(255, 255, 255, 0);
+  WiFi.softAPConfig(ip, ip, subnet);
+  WiFi.softAP(ssid, password);
+  
+  MDNS.begin("esp8266");
+
+  server.on("/", handleRoot);
+  server.on("/data.json", handleData);
+
+  server.begin();
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  server.handleClient();
+  MDNS.update();
+  
   static uint32_t et = 0;
   if ((millis() - et) > MEASUREMENT_PERIOD) {
     et += MEASUREMENT_PERIOD;
     uint8_t e = get_echo();
-    static int p = 0;
     static uint32_t longavg = 0;
     static uint32_t shortavg = 0;
     
@@ -88,8 +178,11 @@ void loop() {
     Serial.print(s_avg[p]);
     if (b) {
       Serial.print(" bounce");
+      bncs[p] = 1;
       if (BEEP) tone(BEEP_PIN, BEEP_FREQV, BEEP_DUR);
     }
+    else
+      bncs[p] = 0;
     Serial.println();
     
     p++;
